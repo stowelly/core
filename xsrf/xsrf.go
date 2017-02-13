9103f3f831ad68179e3cbd4fe6ef8a36a7142af6
mike <michael.stowell84@gmail.com>
2017-02-13 19:48:47 +0000

    csrf token stuff

--- xsrf/xsrf.go
+++ xsrf/xsrf.go
@@ -17,5 +17,4 @@ type Info struct {
 // Token sets token in the template to the CSRF token.
 func Token(w http.ResponseWriter, r *http.Request, v *view.Info) {
 	w.Header().Set("X-CSRF-Token", csrf.Token(r))
-	v.Vars["token"] = csrf.Token(r)
 }
